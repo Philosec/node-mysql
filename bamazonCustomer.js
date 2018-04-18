3af4293600dc0c5cd95a874b5923c1c5c74d23c8
Shane Padgett <spadgett00@gmail.com>
2018-04-18 12:14:20 -0500

    Process order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,10 @@
 'use strict'
 
-let mysql = require('promise-mysql');
-let inquirer = require('inquirer');
-let utils = require('./utils.js')
-let keys = require('./keys.js');
+require('./utils.js')
+
+let mysql = require('promise-mysql')
+let inquirer = require('inquirer')
+let keys = require('./keys.js')
 
 let connection = {}
 
@@ -14,23 +15,37 @@ mysql.createConnection({
   password: keys.db.pass,
   database: 'bamazon'
 }).then(conn => {
-  connection = conn;
-  showAllProducts().then(result => {
-    console.log("")
-    console.logAsTable(result)
-    conn.end()
-  })
+  connection = conn
+  displayAllProducts()
+  processOrder('Chips', 1)
 })
 
-let showAllProducts = () => {
-  return new Promise((resolve, reject) => {
-    let query = `SELECT a.item_id ID, a.product_name Name, a.price Price FROM products a`
-    connection.query(query)
-      .then(result => {
-        resolve(result)
+let displayAllProducts = () => {
+  let query = `SELECT p.item_id ID, p.product_name Name, p.price Price FROM products p`
+  connection.query(query).then(result => {
+    console.log('')
+    console.logAsTable(result)
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+let processOrder = (itemName, qtyRequested) => {
+  let sQuery = `SELECT p.stock_qty, p.price FROM products p WHERE p.product_name LIKE ?`
+  itemName = `%${itemName}%`
+  connection.query(sQuery, itemName).then(sResult => {
+    if (sResult[0].stock_qty >= qtyRequested) {
+      let uQuery = `UPDATE products SET stock_qty = stock_qty - ? WHERE product_name LIKE ?`
+      connection.query(uQuery, [qtyRequested, itemName]).then(uResult => {
+        let totalPrice = (qtyRequested * sResult[0].price).toFixed(2)
+        console.log(`\nProcessed order for a total of $${totalPrice}`)
+      }).catch(err => {
+        console.log(err)
       })
-      .catch(err => {
-        reject(err)
-      })
+    } else {
+      console.log(`\nInsufficient quantity.`)
+    }
+  }).catch(err => {
+    console.log(err)
   })
 }
--- sql/sqlTests.sql
+++ sql/sqlTests.sql
@@ -1,3 +1,9 @@
 USE bamazon;
 
 SELECT a.item_id ID, a.product_name Name, a.price Price FROM products a;
+
+SELECT a.item_id ID, a.product_name Name, a.price Price, a.stock_qty Qty FROM products a;
+
+SELECT p.product_name, p.stock_qty FROM products p WHERE p.product_name LIKE '%Chips%';
+
+UPDATE products SET stock_qty = 12 WHERE product_name LIKE '%Chips%'
