b2e0c29898e5cb38252c228e10ee61782998bbb3
Shane Padgett <spadgett00@gmail.com>
2018-04-18 12:43:14 -0500

    Place orders

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,36 +16,74 @@ mysql.createConnection({
   database: 'bamazon'
 }).then(conn => {
   connection = conn
-  displayAllProducts()
-  processOrder('Chips', 1)
+  placeOrder()
 })
 
-let displayAllProducts = () => {
-  let query = `SELECT p.item_id ID, p.product_name Name, p.price Price FROM products p`
-  connection.query(query).then(result => {
+let placeOrder = () => {
+  displayAllProducts().then(() => {
     console.log('')
-    console.logAsTable(result)
-  }).catch(err => {
-    console.log(err)
+    inquirer.prompt([
+      {
+        name: 'id',
+        type: 'input',
+        message: 'What product id would you like to purchase?',
+        validate: value => {
+          return isNaN(value) === false
+        }
+      },
+      {
+        name: 'qty',
+        type: 'input',
+        message: 'How many would you like?',
+        validate: value => {
+          return isNaN(value) === false
+        }
+      }
+    ]).then(answers => {
+      processOrder(answers.id, answers.qty).then(() => {
+        placeOrder()
+      }).catch(() => {
+        placeOrder()
+      })
+    })
   })
 }
 
-let processOrder = (itemName, qtyRequested) => {
-  let sQuery = `SELECT p.stock_qty, p.price FROM products p WHERE p.product_name LIKE ?`
-  itemName = `%${itemName}%`
-  connection.query(sQuery, itemName).then(sResult => {
-    if (sResult[0].stock_qty >= qtyRequested) {
-      let uQuery = `UPDATE products SET stock_qty = stock_qty - ? WHERE product_name LIKE ?`
-      connection.query(uQuery, [qtyRequested, itemName]).then(uResult => {
-        let totalPrice = (qtyRequested * sResult[0].price).toFixed(2)
-        console.log(`\nProcessed order for a total of $${totalPrice}`)
-      }).catch(err => {
-        console.log(err)
-      })
-    } else {
-      console.log(`\nInsufficient quantity.`)
-    }
-  }).catch(err => {
-    console.log(err)
+let displayAllProducts = () => {
+  return new Promise((resolve, reject) => {
+    let query = `SELECT p.item_id ID, p.product_name Name, p.price Price FROM products p`
+    connection.query(query).then(result => {
+      console.log('')
+      console.logAsTable(result)
+      resolve()
+    }).catch(err => {
+      console.log(err)
+      reject()
+    })
+  })
+}
+
+let processOrder = (productId, qtyRequested) => {
+  return new Promise((resolve, reject) => {
+    let sQuery = `SELECT p.stock_qty, p.price FROM products p WHERE p.item_id = ?`
+    connection.query(sQuery, productId).then(sResult => {
+      if (sResult[0].stock_qty >= qtyRequested) {
+        let uQuery = `UPDATE products SET stock_qty = stock_qty - ? WHERE item_id = ?`
+        connection.query(uQuery, [qtyRequested, productId]).then(uResult => {
+          let totalPrice = (qtyRequested * sResult[0].price).toFixed(2)
+          console.log(`\nProcessed order for a total of $${totalPrice}`)
+          resolve()
+        }).catch(err => {
+          console.log(err)
+          reject(err)
+        })
+      } else {
+        console.log(`\nInsufficient quantity.`)
+        reject()
+      }
+    }).catch(err => {
+      console.log(err)
+      reject()
+    })
   })
 }
