89bd84e0f186c243da9b8b7e85f63692abd8602f
Shane Padgett <spadgett00@gmail.com>
2018-04-20 21:22:20 -0500

    Dynamic department lookup

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,25 +17,18 @@ mysql.createConnection({
   database: 'bamazon'
 }).then(conn => {
   connection = conn
-  // runMenu()
-  g.departments.update(conn)
+  g.updateDeptNames(conn).then(() => runMenu())
 })
 
-const departments = [
-  'Grocery',
-  'Automotive',
-  'Electronics & Office',
-  'Clothing & Shoes',
-  'Home, Furniture & Appliances',
-  'Home Improvement',
-  `Baby & Toddler`,
-  `Toys & Games`,
-  `Sport & Fitness`,
-  `Sewing & Crafts`,
-]
+const queries = {
+  allInvQuery: `SELECT p.item_id ID, p.product_name Name, p.price Price, p.stock_qty Quantity FROM products p`,
+  lowInvQuery: `SELECT p.item_id ID, p.product_name Name, p.price Price, p.stock_qty Quantity FROM products p WHERE p.stock_qty < 5`
+}
 
-const questionBank = {
-  menu: {
+let runMenu = () => {
+  console.log('')
+
+  const question = {
     name: 'action',
     type: 'list',
     message: 'What would you like to do?',
@@ -46,79 +39,23 @@ const questionBank = {
       'Add New Product',
       'Exit'
     ]
-  },
-  increaseItemInv: [
-    {
-      name: 'id',
-      type: 'input',
-      message: 'What product id would you like to increase inventory for?',
-      validate: value => {
-        return isNaN(value) === false
-      }
-    },
-    {
-      name: 'amount',
-      type: 'input',
-      message: 'How many would you like to add?',
-      validate: value => {
-        return isNaN(value) === false
-      }
-    }
-  ],
-  addNewItem: [
-    {
-      name: 'name',
-      type: 'input',
-      message: 'What is the items name?'
-    },
-    {
-      name: 'dept',
-      type: 'list',
-      message: 'What department is the item located in?',
-      choices: departments
-    },
-    {
-      name: 'price',
-      type: 'input',
-      message: 'What is the price of the product?',
-      validate: value => {
-        return isNaN(value) === false
-      }
-    },
-    {
-      name: 'amt',
-      type: 'input',
-      message: 'What is the initial stock quantity?',
-      validate: value => {
-        return isNaN(value) === false
-      }
-    }
-  ]
-}
+  }
 
-const queries = {
-  allInvQuery: `SELECT p.item_id ID, p.product_name Name, p.price Price, p.stock_qty Quantity FROM products p`,
-  lowInvQuery: `SELECT p.item_id ID, p.product_name Name, p.price Price, p.stock_qty Quantity FROM products p WHERE p.stock_qty < 5`
-}
-
-let runMenu = () => {
-  console.log('')
-
-  inquirer.prompt(questionBank.menu).then(answer => {
+  inquirer.prompt(question).then(answer => {
     switch (answer.action) {
-      case questionBank.menu.choices[0]:
+      case question.choices[0]:
         displayQueryAsTable(queries.allInvQuery).then(() => runMenu()).catch(err => console.log(err))
         break
-      case questionBank.menu.choices[1]:
+      case question.choices[1]:
         displayQueryAsTable(queries.lowInvQuery).then(() => runMenu()).catch(err => console.log(err))
         break
-      case questionBank.menu.choices[2]:
+      case question.choices[2]:
         addInventory()
         break
-      case questionBank.menu.choices[3]:
+      case question.choices[3]:
         addNewProduct()
         break
-      case questionBank.menu.choices[4]:
+      case question.choices[4]:
         connection.end()
         process.exit()
         break
@@ -141,7 +78,27 @@ let displayQueryAsTable = query => {
 let addInventory = () => {
   displayQueryAsTable(queries.allInvQuery).then(() => {
     console.log()
-    inquirer.prompt(questionBank.increaseItemInv).then(answer => {
+
+    const question = [
+      {
+        name: 'id',
+        type: 'input',
+        message: 'What product id would you like to increase inventory for?',
+        validate: value => {
+          return isNaN(value) === false
+        }
+      },
+      {
+        name: 'amount',
+        type: 'input',
+        message: 'How many would you like to add?',
+        validate: value => {
+          return isNaN(value) === false
+        }
+      }
+    ]
+
+    inquirer.prompt(question).then(answer => {
       const id = parseInt(answer.id)
       const amt = parseInt(answer.amount)
       const query = `UPDATE products SET stock_qty = stock_qty + ? WHERE item_id = ?`
@@ -155,13 +112,43 @@ let addInventory = () => {
 }
 
 let addNewProduct = () => {
-  inquirer.prompt(questionBank.addNewItem).then(answer => {
+  const question = [
+    {
+      name: 'name',
+      type: 'input',
+      message: 'What is the items name?'
+    },
+    {
+      name: 'dept',
+      type: 'list',
+      message: 'What department is the item located in?',
+      choices: g.deptNames
+    },
+    {
+      name: 'price',
+      type: 'input',
+      message: 'What is the price of the product?',
+      validate: value => {
+        return isNaN(value) === false
+      }
+    },
+    {
+      name: 'amt',
+      type: 'input',
+      message: 'What is the initial stock quantity?',
+      validate: value => {
+        return isNaN(value) === false
+      }
+    }
+  ]
+
+  inquirer.prompt(question).then(answer => {
     const name = answer.name
-    const deptId = departments.indexOf(answer.dept) + 1 //add one to index since foreign key is 1 based
+    const deptId = g.deptNames.indexOf(answer.dept) + 1 //add one to index since foreign key is 1 based
     const price = parseFloat(answer.price)
     const amt = parseInt(answer.amt)
-    const query = `INSERT INTO products (product_name, department_id, price, stock_qty)
-                   VALUES (?, ?, ?, ?)`
+    const query = `INSERT INTO products (product_name, department_id, price, stock_qty, product_sales)
+                   VALUES (?, ?, ?, ?, 0.00)`
     connection.query(query, [name, deptId, price, amt]).then(result => {
       displayQueryAsTable(queries.allInvQuery).then(() => runMenu()).catch(err => console.log(err))
     }).catch(err => console.log(err))
--- globals.js
+++ globals.js
@@ -1,15 +1,16 @@
 'use strict'
 
-require('./utils')
+exports.deptNames = []
 
-let mysql = require('promise-mysql')
-let keys = require('./keys.js')
-
-exports.departments = {
-  update: dbConnection => {
+exports.updateDeptNames = dbConnection => {
+  return new Promise((resolve, reject) => {
     let query = `SELECT d.department_id, d.department_name FROM departments d ORDER BY d.department_id`
     dbConnection.query(query).then(result => {
-      result.forEach(item => { console.log(item.department_id)})
+      exports.deptNames = result.map(obj => {return obj.department_name})
+      resolve()
+    }).catch(err => {
+      console.log(err)
+      reject()
     })
-  }
+  })
 }
